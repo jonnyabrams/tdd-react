88073b67cc447383b8519b853f282af6a2d8f441
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-06 17:16:27 +0100

    Store code complete

--- src/store/restaurants/actions.js
+++ src/store/restaurants/actions.js
@@ -1,1 +1,12 @@
-export const loadRestaurants = () => () => {}
+export const STORE_RESTAURANTS = 'STORE_RESTAURANTS'
+
+export const loadRestaurants = () => async (dispatch, getState, api) => {
+  const records = await api.loadRestaurants()
+
+  const storeRestaurants = records => ({
+    type: STORE_RESTAURANTS,
+    records
+  })
+
+  dispatch(storeRestaurants(records))
+}
