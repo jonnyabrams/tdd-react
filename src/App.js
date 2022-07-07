51af8475c29ca8da84672bc1e6d951d8f0dc03eb
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-07 11:42:17 +0100

    Restyle app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import { Provider } from 'react-redux'
+import {createTheme} from '@mui/material/styles'
+import {green} from '@mui/material/colors'
+import {ThemeProvider} from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
 import store from './store'
 import RestaurantScreen from "./components/RestaurantScreen"
 
+const theme = createTheme({
+  palette: {
+    primary: green
+  }
+})
 
-function App() {
+const App = () => {
   return (
     <Provider store={store}>
-      <RestaurantScreen />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">Food Bastard</Typography>
+          </Toolbar>
+        </AppBar>
+        <Container>
+           <RestaurantScreen />
+        </Container>
+      </ThemeProvider>
     </Provider>
-  
   );
 }
 
