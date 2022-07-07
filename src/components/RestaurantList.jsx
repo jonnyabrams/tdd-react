eb75c484d0bd144a3cf8474ddb020092b64e1d38
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-07 12:02:10 +0100

    Pass tests for loading icon

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -1,23 +1,27 @@
 import { useEffect } from "react"
 import { connect } from 'react-redux'
+import CircularProgress from '@mui/material/CircularProgress'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemText from '@mui/material/ListItemText'
 import { loadRestaurants } from '../store/restaurants/actions'
 
-export const RestaurantList = ({ loadRestaurants, restaurants }) => {
+export const RestaurantList = ({ loadRestaurants, restaurants, loading }) => {
   useEffect(() => {
     loadRestaurants()
   }, [loadRestaurants])
 
   return (
-    <List>
-      {restaurants.map(restaurant => (
-        <ListItem key={restaurant.id}>
-          <ListItemText>{restaurant.name}</ListItemText>
-        </ListItem>
-      ))}
-    </List>
+    <>
+     {loading && <CircularProgress />}
+      <List>
+        {restaurants.map(restaurant => (
+          <ListItem key={restaurant.id}>
+            <ListItemText>{restaurant.name}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
+    </>
   )
 }
 
--- src/components/RestaurantList.spec.js
+++ src/components/RestaurantList.spec.js
@@ -9,13 +9,17 @@ describe('RestaurantList', () => {
 
   let loadRestaurants
 
-  const renderComponent = () => {
-    loadRestaurants = jest.fn().mockName('loadRestaurants')
+  const renderComponent = (propOverrides = {}) => {
+    const props = {
+      loadRestaurants: jest.fn().mockName('loadRestaurants'),
+      restaurants, ...propOverrides
+    }
+
+    loadRestaurants = props.loadRestaurants
 
     render(
       <RestaurantList 
-        loadRestaurants={loadRestaurants} 
-        restaurants={restaurants}
+        {...props}
       />
     )
   }
@@ -31,5 +35,17 @@ describe('RestaurantList', () => {
 
     expect(screen.getByText('Sushi Place')).toBeInTheDocument()
     expect(screen.getByText('Pizza Place')).toBeInTheDocument()
+  })
+
+  it('displays the loading indicator while loading', () => {
+    renderComponent({loading: true})
+    // use the 'progressbar' ARIA role
+    expect(screen.getByRole('progressbar')).toBeInTheDocument()
+  });
+
+  it('does not display the loading indicator while not loading', () => {
+    renderComponent({loading: false})
+    expect(screen.queryByRole('progressbar')).not.toBeInTheDocument()
+    // query methods return a null if an element is not found, whereas get methods throw an error. Because we expect to not find the element here, a query method is necessary for our assertion to succeed.
   })
 })
