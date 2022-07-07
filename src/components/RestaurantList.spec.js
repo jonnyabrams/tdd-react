6b609f3a7670b67d6cbfcd97987c54820708b773
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-07 12:04:14 +0100

    Refactor tests with describe block

--- src/components/RestaurantList.spec.js
+++ src/components/RestaurantList.spec.js
@@ -30,22 +30,24 @@ describe('RestaurantList', () => {
     expect(loadRestaurants).toHaveBeenCalled()
   })
 
-  it('displays the restaurants', () => {
-    renderComponent()
-
-    expect(screen.getByText('Sushi Place')).toBeInTheDocument()
-    expect(screen.getByText('Pizza Place')).toBeInTheDocument()
-  })
-
   it('displays the loading indicator while loading', () => {
     renderComponent({loading: true})
     // use the 'progressbar' ARIA role
     expect(screen.getByRole('progressbar')).toBeInTheDocument()
-  });
+  })
 
-  it('does not display the loading indicator while not loading', () => {
-    renderComponent({loading: false})
-    expect(screen.queryByRole('progressbar')).not.toBeInTheDocument()
-    // query methods return a null if an element is not found, whereas get methods throw an error. Because we expect to not find the element here, a query method is necessary for our assertion to succeed.
+  describe('when loading succeeds', () => {
+    it('displays the restaurants', () => {
+      renderComponent()
+  
+      expect(screen.getByText('Sushi Place')).toBeInTheDocument()
+      expect(screen.getByText('Pizza Place')).toBeInTheDocument()
+    })
+  
+    it('does not display the loading indicator while not loading', () => {
+      renderComponent({loading: false})
+      expect(screen.queryByRole('progressbar')).not.toBeInTheDocument()
+      // query methods return a null if an element is not found, whereas get methods throw an error. Because we expect to not find the element here, a query method is necessary for our assertion to succeed.
+    })
   })
 })
